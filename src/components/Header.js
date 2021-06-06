a93662a2ce873889f9e2bb20b2abe1c995c6e194
majdkh97 <majd.motaz@yahoo.com>
2021-06-06 17:11:37 +0300

    lab01

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Header extends React.Component {
+
+    render() {
+        return (
+            <header>
+                <h1>Horned-Beasts</h1>
+            </header>
+        )
+    }
+}
+
+// export makes this file visible to other files/ components
+export default Header;
---
+++ src/components/HornedBeasts.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class HornedBeasts extends React.Component {
+
+    render() {
+        return (
+            <header>
+                <h1>Horned-Beasts</h1>
+            </header>
+        )
+    }
+}
+
+// export makes this file visible to other files/ components
+export default HornedBeasts;
