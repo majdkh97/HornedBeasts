f9252e5fd89c809b12302ab5c14ee27396635abc
majdkh97 <majd.motaz@yahoo.com>
2021-06-08 17:19:22 +0300

    kms

--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
 
 
 class HornedBeasts extends React.Component {
@@ -16,15 +19,30 @@ class HornedBeasts extends React.Component {
     }
 
     render() {
+    
         return (
-            <header>
-                <h1>{this.props.title}</h1>
-                <img src={this.props.image_url} alt="" onClick={this.Voting}/>
-                <p>{this.props.description}</p>
-                <button onClick={this.Voting}>
-                    &hearts;{this.state.amountOfVotes}
-                </button>
-            </header>
+
+            // <header>
+            //     <h1>{this.props.title}</h1>
+            //     <img src={this.props.image_url} alt="" onClick={this.Voting}/>
+            //     <p>{this.props.description}</p>
+            //     <button onClick={this.Voting}>
+            //         &hearts;{this.state.amountOfVotes}
+            //     </button>
+            // </header>
+
+
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={this.props.image_url} onClick={this.Voting} />
+                <Card.Body>
+                    <Card.Title>{this.props.title}</Card.Title>
+                    <Card.Text>
+                        {this.props.description}
+                    </Card.Text>
+                    <Button onClick={this.Voting} variant="primary">&hearts;{this.state.amountOfVotes}</Button>
+                </Card.Body>
+            </Card>
+
         )
     }
 }
